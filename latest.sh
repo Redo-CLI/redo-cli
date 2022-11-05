10227c60b90abf73da6537df72e3960b57fd8f9d
Anand Singh <sudoanand@Anands-MacBook-Air.local>
2022-11-05 15:42:35 +0530

    Rename files

--- latest.sh
+++ latest.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "Installing redo..."
-curl -s https://redo.sh/cli/redo > /usr/local/bin/redo
+curl -s https://raw.githubusercontent.com/redo-cli/redo-cli/master/redo.sh > /usr/local/bin/redo
 chmod +x /usr/local/bin/redo
 echo "Installation complete."
 echo "You are ready to do more without leaving the terminal!"
