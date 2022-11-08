5c98d2d97d8d19b729bb98120757917fbc676b14
Anand Singh <sudoanand@Anands-MacBook-Air.local>
2022-11-08 09:55:45 +0530

    Fix installer for ubuntu

--- latest.sh
+++ latest.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 echo "Installing redo..."
-curl -s https://raw.githubusercontent.com/redo-cli/redo-cli/master/redo.sh > /usr/local/bin/redo
+echo "Installing in: /usr/local/bin"
+curl -s https://raw.githubusercontent.com/redo-cli/redo-cli/master/redo.sh > /tmp/redo.sh
+mv /tmp/redo.sh /usr/local/bin/redo
 chmod +x /usr/local/bin/redo
 echo "Installation complete."
 echo "You are ready to do more without leaving the terminal!"
