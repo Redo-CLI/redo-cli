9cf1862dcd436038c20a504fd4b6150a0adade3a
Anand Singh <sudoanand@Anands-MacBook-Air.local>
2022-11-10 11:35:50 +0530

    Update installed: sudo when needed

--- latest.sh
+++ latest.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
+INSTALLER_VERSION="1.0.1"
+echo "Installer version: v"$INSTALLER_VERSION
 echo "Installing redo..."
 echo "Installing in: /usr/local/bin"
 curl -s https://raw.githubusercontent.com/redo-cli/redo-cli/master/redo.sh > /tmp/redo.sh
-mv /tmp/redo.sh /usr/local/bin/redo
-chmod +x /usr/local/bin/redo
+mv /tmp/redo.sh /usr/local/bin/redo 2>/dev/null
+chmod +x /usr/local/bin/redo 2>/dev/null
+if [ -e "/usr/local/bin/redo" ]
+then
+	"Done!"
+else
+	sudo mv /tmp/redo.sh /usr/local/bin/redo 
+	sudo chmod +x /usr/local/bin/redo
+fi
+
 echo "Installation complete."
 echo "You are ready to do more without leaving the terminal!"
